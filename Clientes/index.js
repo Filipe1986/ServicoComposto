43e2bcc2ad2b6a1cef23b587bb5c316a55972815
villaca <danielvillacasilva@gmail.com>
2017-06-07 01:19:58 -0300

    + form basicão para inserir autor + enunciado do trabalho para ficar acessível (ToDo: transformar num readme.md) +/# requisições axios ajeitadas para a rota supostamente correta

--- Clientes/index.js
+++ Clientes/index.js
@@ -4,14 +4,23 @@ Vue.use(Vuetable);
 var app = new Vue({
     el: '#app',
     data: {
-        fields: ['cpf', 'nome']
+        fields: ['cpf', 'nome'],
+        cpf: "",
+        nome: ""
     },
     methods: {
         getAutores(){
-            this.$http.get("http://127.0.0.1:5000/").then((response) => {
+            this.$http.get("http://127.0.0.1:5000/autor/").then((response) => {
                 console.log(response.data)
-
             });
+        },
+        addAutor(){
+            console.log("cpf: " + this.cpf);
+            console.log("nome: " + this.nome);
+            this.$http.post("http://127.0.0.1:5000/autor/", {
+                cpf : this.cpf,
+                nome : this.nome
+            }).then((response) => {});
         }
     }
 });
